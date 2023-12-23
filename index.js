0cf7db506d2327047b3e2508a85348381d24e3c6
asmaulhosen45 <asmaulhosen45@gmail.com>
2023-12-23 19:45:21 +0600

    solve Cors Error

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require("express");
 require("dotenv").config();
-const app = express()
-const port = 3000
+const cors = require("cors");
+const app = express();
+const port = 3000;
 
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://task-flow-asmaul.web.app",
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `${process.env.DB_URI}`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -14,10 +25,10 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-const taskCollection = client.db("TaskFlow").collection("AllTask");
+const taskCollection = client.db("TaskFlow").collection("Tasks");
 
 async function run() {
   try {
@@ -33,11 +44,49 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("TaskFlow is Running ....");
+});
 
-app.get('/', (req, res) => {
-  res.send('TaskFlow is Running ....')
-})
+// ===== CRUD operation =====
+
+// ----- Read Operation -----
+app.get("/tasks", async (req, res) => {
+  const result = await taskCollection.find().toArray();
+  res.send(result);
+});
+
+// ----- Post Operation -----
+app.post("/task", async (req, res) => {
+  const task = req.body;
+  console.log(task);
+  const result = await taskCollection.insertOne(task);
+  console.log(result);
+  res.send(result);
+});
+
+// ----- Update Operation -----
+app.patch("/task/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  const filter = { _id: new ObjectId(id) };
+  const updatedData = {
+    $set: {
+      status: data.taskName,
+    },
+  };
+  const result = await taskCollection.updateOne(filter, updatedData);
+  res.send(result);
+});
+
+// ----- delete Operation -----
+app.delete("/task/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await taskCollection.deleteOne(query);
+  res.send(result);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
